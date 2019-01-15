58ecd58df6c3f20475fb28b96f44f6e8ead6c6f7
darekem73 <darek.mastalerz@gmail.com>
2019-01-15 22:08:39 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -284,7 +284,7 @@ function displayKeyboard(letter) {
       rect(x, y, letterW, letterH);
       noStroke();
       fill('black');
-      textSize(max(letterW / 2, 48));
+      textSize(min(letterW / 2, 42));
       text(letters[i][j], x + letterW / 2, y + letterH / 1.3);
       let b = {
         tl: {
